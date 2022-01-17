b1d4461202ad4632bfaa6a590624cefa1ade3162
naufalrafih <naufal.rafi@accelbyte.net>
2022-01-17 21:50:21 +0700

    test http request

--- raspberrypi/connectivity-test/app.py
+++ raspberrypi/connectivity-test/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+ADVERSARY_HOST="raspberrypi3"
+ADVERSARY_PORT="5000"
+
 @app.route("/")
 def hello_world():
-    return("Hi ESP8266! I'm up.")
+    return("Connection Successful!")
+
+@app.route("/request")
+def request():
+    r = requests.get(f'http://{ADVERSARY_HOST}:{ADVERSARY_PORT}/')
+    print("Status code:",r.status_code)
+    print("Body:",r.content)
