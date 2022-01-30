7062f7030795164f954f140f0c84328222a1e1ed
naufalrafih <naufal.rafi@accelbyte.net>
2022-01-30 13:38:48 +0700

    api/initialized

--- raspberrypi/mechanism/certcenter/app.py
+++ raspberrypi/mechanism/certcenter/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import requests
+import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -16,6 +18,27 @@ def request():
     print("Status code:",r.status_code)
     print("Body:",r.content)
     return("Request Successful!")
+
+@app.route("/api/initialize")
+def initialize():
+    con = sqlite3.connect('cert-center.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM certcenter')
+    row = cur.fetchall()
+    print(row)
+    if not row:
+        print("table is empty")
+        certcenter_name = "certcenter"
+        certcenter_ip_address = "10.0.0.3"
+        key_b = os.urandom(6).hex()
+        cur.execute("INSERT INTO certcenter (certcenter_name, certcenter_ip_address, key_b) VALUES (?, ?, ?)",
+            (certcenter_name, certcenter_ip_address, key_b))
+    else:
+        print("table is not empty")
+        return("already initialized")
+    con.commit()
+    con.close()
+    return("initialized")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',ssl_context=('cert.pem','key.pem'),debug=True)
