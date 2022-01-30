8cd60f4a1b1431fb03531004336cadab9387f370
Fajri Kornel <fajri.apk@gmail.com>
2022-01-30 13:08:12 +0700

    Added error handling for current APIs

--- raspberrypi/mechanism/certcenter/app.py
+++ raspberrypi/mechanism/certcenter/app.py
@@ -6,18 +6,27 @@ app = Flask(__name__)
 
 @app.route("/home")
 def home_page():
-    con = sqlite3.connect("cert-center.db")
-    cur = con.cursor()
-
-    if (cur.execute('SELECT * FROM certcenter')):
-        initialized=True
-    else:
-        initialized=False
-    return render_template('home.html', initialized=initialized)
+    try:
+        con = sqlite3.connect("cert-center.db")
+        cur = con.cursor()
+        if (cur.execute('SELECT * FROM certcenter')):
+            initialized=True
+        else:
+            initialized=False
+        response_text = render_template('home.html', initialized=initialized)
+        response_code = 200
+        return response_text, response_code
+    except Exception as e:
+        return f"Error! Exception: {e}", 500
 
 @app.route("/api/initialize")
 def initialize_cert_center():
-    return ("OK")
+    try:
+        response_text = "OK"
+        response_code = 200
+        return response_text, response_code
+    except Exception as e:
+        return f"Error! Exception: {e}", 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',ssl_context=('cert.pem','key.pem'),debug=True)
--- raspberrypi/mechanism/institute/app.py
+++ raspberrypi/mechanism/institute/app.py
@@ -8,7 +8,13 @@ server_directory = os.path.dirname(os.path.realpath(__file__))
 
 @app.route("/home")
 def hello_world():
-    return render_template("home.html")
+    try:
+        response_text = render_template("home.html")
+        response_code = 200
+        return response_text, response_code
+    except Exception as e:
+        return f"Error! Exception: {e}", 500
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',ssl_context=('cert.pem','key.pem'),debug=True)
