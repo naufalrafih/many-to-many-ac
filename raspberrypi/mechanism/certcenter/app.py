2907b7db5dc3426a22373176dfe3f5bd133806f5
naufalrafih <naufal.rafi@accelbyte.net>
2022-01-30 13:43:58 +0700

    merge conflict

--- raspberrypi/mechanism/certcenter/app.py
+++ raspberrypi/mechanism/certcenter/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 import sqlite3
 import requests
-import sqlite3
 import os
 
 app = Flask(__name__)
@@ -24,32 +23,29 @@ def home_page():
 @app.route("/api/initialize")
 def initialize_cert_center():
     try:
-        response_text = "OK"
-        response_code = 200
+        con = sqlite3.connect('cert-center.db')
+        cur = con.cursor()
+        cur.execute('SELECT * FROM certcenter')
+        row = cur.fetchall()
+        print(row)
+        if not row:
+            print("table is empty")
+            certcenter_name = "certcenter"
+            certcenter_ip_address = "10.0.0.3"
+            key_b = os.urandom(6).hex()
+            cur.execute("INSERT INTO certcenter (certcenter_name, certcenter_ip_address, key_b) VALUES (?, ?, ?)",
+                (certcenter_name, certcenter_ip_address, key_b))    
+            response_text = "Initialized"
+            response_code = 200
+        else:
+            print("table is not empty")
+            response_text = "Already initialized"
+            response_code = 500
+        con.commit()
+        con.close()
         return response_text, response_code
     except Exception as e:
         return f"Error! Exception: {e}", 500
-
-@app.route("/api/initialize")
-def initialize():
-    con = sqlite3.connect('cert-center.db')
-    cur = con.cursor()
-    cur.execute('SELECT * FROM certcenter')
-    row = cur.fetchall()
-    print(row)
-    if not row:
-        print("table is empty")
-        certcenter_name = "certcenter"
-        certcenter_ip_address = "10.0.0.3"
-        key_b = os.urandom(6).hex()
-        cur.execute("INSERT INTO certcenter (certcenter_name, certcenter_ip_address, key_b) VALUES (?, ?, ?)",
-            (certcenter_name, certcenter_ip_address, key_b))
-    else:
-        print("table is not empty")
-        return("already initialized")
-    con.commit()
-    con.close()
-    return("initialized")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',ssl_context=('cert.pem','key.pem'),debug=True)
