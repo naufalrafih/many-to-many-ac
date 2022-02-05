36703043066f5ee156484a172c6a5614f9d06e05
Fajri Kornel <fajri.apk@gmail.com>
2022-02-05 17:11:19 +0700

    Modified APIs; testing mechanism 1 done

--- raspberrypi/mechanism/institute/app.py
+++ raspberrypi/mechanism/institute/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
 import requests
 import sqlite3
-import os
-import json
 from Crypto.PublicKey import RSA
+import time
 
 app = Flask(__name__)
 
@@ -25,7 +24,7 @@ def initialize_institute():
     try:
         con = sqlite3.connect("db/institute-server.db")
         cur = con.cursor()
-        if (cur.execute("SELECT * FROM certcenter").fetchall()):
+        if ((cur.execute("SELECT * FROM certcenter").fetchall()) and (cur.execute("SELECT * FROM institute").fetchall())):
             initialized = True
         else:
             initialized = False
@@ -40,6 +39,7 @@ def initialize_institute():
 @app.route("/api/initialize", methods=["POST"])
 def api_initialize_institute():
     try:
+        print(f"INITIALIZE CALLED: {time.strftime('%H:%M:%S')}")
         data = request.get_json()
         certcenter_ip_address = data["certcenter_ip_address"]
         institute_name = data["institute_name"]
@@ -52,22 +52,25 @@ def api_initialize_institute():
                     ("Cert Center", certcenter_ip_address))
 
         #Send request to Cert Center API /api/register/institute
-        request_body = json.dumps({"institute_name":institute_name})
-        response = requests.get(f"https://{certcenter_ip_address}:{CERTCENTER_PORT}/api/register/institute", verify="certs/certcenter.pem", json=request_body)
-        if (response.status_code == 200):
+        request_body = {"institute_name":institute_name}
+        print(f"REQUEST TO CERTCENTER: {time.strftime('%H:%M:%S')}")
+        response = requests.post(f"https://{certcenter_ip_address}:{CERTCENTER_PORT}/api/register/institute", verify="certs/certcenter.pem", json=request_body)
+        if (response.status_code == 200):        
+            print(f"RESPONSE FROM CERTCENTER: {time.strftime('%H:%M:%S')}")
             response_json = response.json()
             private_key = RSA.generate(2048)
             public_key = private_key.public_key()
 
             #Variables to insert into DB
+            institute_id = response_json["institute_id"]
             institute_ip_address = response_json["institute_ip_address"]
             private_key_pem = private_key.export_key("PEM")
             public_key_pem = public_key.export_key("PEM")
             key_a = response_json["key_a"]
 
             cur.execute("DELETE FROM institute")
-            cur.execute("INSERT INTO institute (institute_name, institute_ip_address, public_key, private_key, key_a) VALUES (?, ?, ?, ?, ?)",
-                        (institute_name, institute_ip_address, public_key_pem, private_key_pem, key_a))
+            cur.execute("INSERT INTO institute (institute_id, institute_name, institute_ip_address, public_key, private_key, key_a) VALUES (?, ?, ?, ?, ?, ?)",
+                        (institute_id, institute_name, institute_ip_address, public_key_pem, private_key_pem, key_a))
         else:
             raise Exception("Request to Cert Center API /api/register/institute failed")
 
@@ -75,6 +78,7 @@ def api_initialize_institute():
         con.close()
         response_text = "Successful."
         response_code = 200
+        print(f"DONE: {time.strftime('%H:%M:%S')}")
         return response_text, response_code
     except Exception as e:
         print(f"Error! Exception: {e}")
@@ -104,7 +108,7 @@ def register_asset():
         con.commit()
         con.close()
 
-        response_text = json.dumps({"key_a":key_a})
+        response_text = {"key_a":key_a}
         response_code = 200
         return response_text, response_code
     except Exception as e:
