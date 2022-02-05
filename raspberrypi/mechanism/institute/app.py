060da540351f08b3c1d33082d6051c52687649e3
naufalrafih <naufal.rafi@accelbyte.net>
2022-02-05 14:42:58 +0700

    update paths

--- raspberrypi/mechanism/institute/app.py
+++ raspberrypi/mechanism/institute/app.py
@@ -55,79 +55,9 @@ def api_initialize_institute():
         print(f"Error! Exception: {e}")
         return f"Unsuccessful", 500
 
-@app.route("/api/initialize/certcenter", methods=["POST"])
-def initialize_certcenter():
-    try:
-        # Create cert center data
-        data = request.get_json()
-        certcenter_name = data["certcenter_name"]
-        certcenter_ip_address = data["certcenter_ip_address"]
-        print(f"Cert Center - Name: {certcenter_name}, IP: {certcenter_ip_address}")
-
-        # Insert into DB
-        con = sqlite3.connect("db/institute-server.db")
-        cur = con.cursor()
-        cur.execute("DELETE FROM certcenter")
-        cur.execute("INSERT INTO certcenter (certcenter_name, certcenter_ip_address) VALUES (?, ?)",
-                        (certcenter_name, certcenter_ip_address))
-        con.commit()
-        con.close()
-
-        response_text = "OK!"
-        response_code = 200
-        return response_text, response_code
-    except Exception as e:
-        print(f"Error! Exception: {e}")
-        return f"Unsuccessful", 500
-
-@app.route("/api/initialize/assets", methods=["POST"])
-def initialize_asset():
-    try:
-        # Create cert center data
-        data = request.get_json()
-
-        # Insert into DB
-        con = sqlite3.connect("db/institute-server.db")
-        cur = con.cursor()
-        cur.execute("DELETE FROM assets")
-        for index, asset in enumerate(data):
-            print(f"Asset no. {index+1} - Name: {asset['asset_name']}, IP: {asset['asset_ip_address']}. Inserting into DB.")
-            cur.execute("INSERT INTO assets (asset_name, asset_ip_address) VALUES (?, ?)",
-                        (asset['asset_name'], asset['asset_ip_address']))
-        con.commit()
-        con.close()
-
-        response_text = "OK!"
-        response_code = 200
-        return response_text, response_code
-    except Exception as e:
-        print(f"Error! Exception: {e}")
-        return f"Unsuccessful", 500
-
-@app.route("/api/register", methods=["POST"])
+@app.route("/api/register/asset", methods=["POST"])
 def register_institute():
     try:
-        #Parse data
-        data = request.get_json()
-        institute_id = data["institute_id"]
-        institute_name = data["institute_name"]
-        institute_ip_address = data["institute_ip_address"]
-        key_a = data["key_a"]
-
-        #Generate public & private key
-        private_key = RSA.generate(2048)
-        public_key = private_key.public_key()
-        private_key_pem = private_key.export_key("PEM").decode("utf-8")
-        public_key_pem = public_key.export_key("PEM").decode("utf-8")
-
-        #Insert into DB
-        con = sqlite3.connect("db/institute-server.sql")
-        cur = con.cursor()
-        cur.execute("INSERT INTO institute (institute_id, institute_name, institute_ip_address, public_key, private_key, key_a) VALUES (?, ?, ?, ?, ?, ?)",
-                    (institute_id, institute_name, institute_ip_address, public_key_pem, private_key_pem, key_a))
-        con.commit()
-        con.close()
-
         response_text = "OK!"
         response_code = 200
         return response_text, response_code
