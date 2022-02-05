cb790e493e6af2ad1eff9af27f4b5d9b50e52fc4
Fajri Kornel <fajri.apk@gmail.com>
2022-02-05 15:26:00 +0700

    Institute API /home/initialize and /api/register/asset

--- raspberrypi/mechanism/institute/app.py
+++ raspberrypi/mechanism/institute/app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, request
-import requests
 import sqlite3
-import os
 from Crypto.PublicKey import RSA
+import json
 
 app = Flask(__name__)
 
 CERTCENTER_PORT=35753
 INSTITUTE_PORT=35754
-server_directory = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/home", methods=["GET"])
 def hello_world():
@@ -40,25 +38,36 @@ def initialize_institute():
 @app.route("/api/initialize", methods=["POST"])
 def api_initialize_institute():
     try:
-        data = request.get_json()
-        certcenter_data = data["certcenter_data"]
-        assets_data = data["assets_data"]
-
-        requests.packages.urllib3.disable_warnings()
-        r_certcenter = requests.post(request.url_root + "api/initialize/certcenter", verify=f"{server_directory}/secrets/cert.pem", json=certcenter_data)
-        r_institute = requests.post(request.url_root + "api/initialize/assets", verify=f"{server_directory}/secrets/cert.pem", json=assets_data)
-        if (r_certcenter.ok and r_institute.ok):
-            response_text = "OK!"
-            response_code = 200
-            return response_text, response_code
+        return "OK."
     except Exception as e:
         print(f"Error! Exception: {e}")
         return f"Unsuccessful", 500
 
 @app.route("/api/register/asset", methods=["POST"])
-def register_institute():
+def register_asset():
     try:
-        response_text = "OK!"
+        data = request.get_json()
+        asset_name = data["asset_name"]
+        asset_ip_address = request.remote_addr
+
+        print("Registering asset:")
+        print(f"Asset name: {asset_name}")
+        print(f"Asset IP address: {asset_ip_address}")
+
+        #Inserting into database
+        con = sqlite3.connect("db/institute-server.db")
+        cur = con.cursor()
+        cur.execute("REPLACE INTO assets (asset_id, asset_name, asset_ip_address) VALUES ((SELECT asset_id FROM assets WHERE asset_name = ?), ?, ?)",
+                    (asset_name, asset_name, asset_ip_address))
+
+        #Getting key_A for the reader
+        cur.execute("SELECT key_a FROM institute")
+        key_a = cur.fetchall()[0][0]
+
+        con.commit()
+        con.close()
+
+        response_text = json.dumps({"key_a":key_a})
         response_code = 200
         return response_text, response_code
     except Exception as e:
