3a5b02b45ce39727bd36d39bacb876284c611702
Fajri Kornel <fajri.apk@gmail.com>
2022-03-27 16:01:47 +0700

    Initial mech4 commit

---
+++ esp8266/mechanism/mech4/functions.h
@@ -0,0 +1,98 @@
+void setNTP() {
+    // Set time via NTP, as required for x.509 validation
+    configTime(3 * 3600, 0, "time.nist.gov", "pool.ntp.org");
+    Serial.print("Waiting for NTP time sync: ");
+    time_t now = time(nullptr);
+
+    while (now < 8 * 3600 * 2) {
+        delay(500);
+        Serial.print(".");
+        now = time(nullptr);
+    }
+
+    Serial.println("");
+
+    struct tm timeinfo;
+
+    gmtime_r(&now, &timeinfo);
+
+    Serial.print("Current time: ");
+    Serial.print(asctime(&timeinfo));
+}
+
+int request(const char *method, const char *path, JsonDocument& doc_request, JsonDocument& doc_response) {
+    // 200: Successful
+    // -1: Connection failed
+    // -2: Deserialize failed
+    BearSSL::WiFiClientSecure wifiClient;
+    wifiClient.setFingerprint(fp);
+
+    setNTP();
+
+    bool is_https = true;
+    HTTPClient https;
+
+    if (https.begin(wifiClient, hostserver, hostport, path, is_https)) {
+        Serial.printf("[HTTPS] %s...\n", (String) method);
+        // start connection and send HTTP header
+
+        String request_body;
+        serializeJson(doc_request, request_body);
+
+        https.addHeader("Content-Type", "application/json");
+        int httpCode;
+        if (method == "GET") {
+            httpCode = https.GET();
+        } else if (method == "POST") {
+            httpCode = https.POST(request_body);
+        }
+
+        // httpCode will be negative on error
+        if (httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTPS] %s... code: %d\n", (String) method, httpCode);
+
+            // file found at server
+            if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+                String payload = https.getString();
+                DeserializationError error = deserializeJson(doc_response, payload);
+                if (error) {
+                    Serial.print(F("deserializeJson() failed: "));
+                    Serial.println(error.f_str());
+                    return -2;
+                }
+                return 200;
+            }
+        } else {
+            Serial.printf("[HTTPS] %s... failed, error: %s\n", (String) method, https.errorToString(httpCode).c_str());
+            return httpCode;
+        }
+    }
+}
+
+void register_asset(char **institute_key, String asset_name) {
+    
+    //Request body
+    StaticJsonDocument<48> doc_request;
+    doc_request["asset_name"] = asset_name;
+
+    //Response body
+    StaticJsonDocument<48> doc_response;
+
+    //Request
+    int response_code = request("POST", "/api/register/asset", doc_request, doc_response);
+    if (response_code == 200) {
+        Serial.println("Success registering and getting institute_key.");
+    } else {
+        Serial.println("Unsuccessful registering.");
+        const char * temp = "FAILED";
+        *institute_key = strdup(temp);
+        return;
+    }
+
+    //Extract variable from response
+    const char * temp = doc_response["institute_key"];
+    *institute_key = strdup(temp);
+    Serial.printf("institute_key: %s\n", *institute_key);
+    return;
+}
---
+++ esp8266/mechanism/mech4/mech4.ino
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid     = "mywifi";
+const char* password = "543216789";
+const char* hostserver = "192.168.137.1";
+//IPAddress hostserver(192, 168, 137, 1);
+uint16_t hostport = 35754;
+
+static const char fp[] PROGMEM = "ED:3F:4A:CC:DB:DB:57:A5:C8:39:AE:65:D7:59:33:94:0E:1D:56:E0";
+#include "functions.h"
+
+char * institute_key;
+String asset_name = "rock";
+
+void setup() {
+    Serial.begin(115200);
+    delay(10);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to ");
+    Serial.print(ssid); Serial.println(" ...");
+
+    int i = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(++i); Serial.print(' ');
+    }
+
+    Serial.println('\n');
+    Serial.println("Connection established!");
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+
+    register_asset(&institute_key, asset_name);
+    while ((String) institute_key == "FAILED") {
+        delay(1000);
+        Serial.println("Try getting key A again...");
+        register_asset(&institute_key, asset_name);
+    }
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        int c = Serial.read();
+        Serial.println("Pressed.");
+        Serial.print("Institute_Key: "); Serial.println(institute_key);
+    }
+}
